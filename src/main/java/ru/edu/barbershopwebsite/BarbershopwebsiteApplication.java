4d20e26ff11801fa39dc5835357abdcab8cdd622
Alex Sadikov <sav1982@inbox.ru>
2022-04-13 17:51:39 +0300

    add package entity

--- src/main/java/ru/edu/barbershopwebsite/BarbershopwebsiteApplication.java
+++ src/main/java/ru/edu/barbershopwebsite/BarbershopwebsiteApplication.java
@@ -1,4 +1,4 @@
-package ru.edu;
+package ru.edu.barbershopwebsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/ru/edu/barbershopwebsite/entity/User.java
@@ -0,0 +1,6 @@
+package ru.edu.barbershopwebsite.entity;
+
+
+public class User {
+
+}
