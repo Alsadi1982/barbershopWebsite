b493ecd35bf48c53375e994d4f6282fb528da77b
ArtemMikishev <artemmikishev94@gmail.com>
2022-04-13 20:18:42 +0300

    add user entity and User Role and application.properties

--- src/main/java/ru/edu/barbershopwebsite/entity/User.java
+++ src/main/java/ru/edu/barbershopwebsite/entity/User.java
@@ -1,6 +1,46 @@
 package ru.edu.barbershopwebsite.entity;
 
 
-public class User {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+import javax.persistence.*;
+
+/**
+ * Сущность клиента(пользователя, барбера, админа)
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Enumerated(EnumType.ORDINAL)
+    private UserRole role = UserRole.USER;
+
+    @Override
+    public String toString() {
+        return login;
+    }
+//
+//    public String getGreet() {
+//        return firstName;
+//    }
 }
+
