29e8cf13bc931b8abeeccedb081e267777fdb1d9
Alex Sadikov <sav1982@inbox.ru>
2022-04-13 15:41:04 +0300

    get start project

---
+++ src/main/java/ru/edu/BarbershopwebsiteApplication.java
@@ -0,0 +1,13 @@
+package ru.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbershopwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbershopwebsiteApplication.class, args);
+	}
+
+}
